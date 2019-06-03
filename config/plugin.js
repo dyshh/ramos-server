59a901ff1dbad460b6ff13f95ce01f12a575617f
pengkaiyang <476922814@qq.com>
2019-06-03 23:36:28 +0800

    lint

--- config/plugin.js
+++ config/plugin.js
@@ -4,14 +4,14 @@
 module.exports = {
     io: {
         enable: true,
-        package: 'egg-socket.io',
+        package: 'egg-socket.io'
     },
     mysql: {
         enable: true,
-        package: 'egg-mysql',
+        package: 'egg-mysql'
     },
     jwt: {
         enable: true,
-        package: 'egg-jwt',
-    },
+        package: 'egg-jwt'
+    }
 };
